4e2bd4102d33ddf3e26bdcd9e082d6a643a49b08
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-09-22 15:03:22 +0200

    Prepare for workshop

--- elmish/src/Client/Question/Show/State.fs
+++ elmish/src/Client/Question/Show/State.fs
@@ -7,50 +7,9 @@ open Okular.Operators
 module Validation = Shared.Validation.Question
 
 let init id  =
-    Model.Empty, Cmd.ofMsg (FetchDetail id)
+    Model.Empty, Cmd.none
 
 let update msg model =
     match msg with
-    | NetworkError error ->
-        printfn "[Question.Show.State][Network error] %s" error.Message
+    | None ->
         model, Cmd.none
-
-    | FetchDetail id ->
-        model, Cmd.ofPromise Rest.getDetails id FetchDetailSuccess NetworkError
-
-    | FetchDetailSuccess result ->
-        { model with Question = Some result.Question
-                     Answers = result.Answsers }, Cmd.none
-
-    | ChangeReply value ->
-        model
-        |> Lens.set (Model.ReplyLens >-> StringField.ValueLens) value
-        |> Lens.set (Model.ReplyLens >-> StringField.ErrorLens) None, Cmd.none
-
-    | Submit ->
-        if model.IsWaitingReply then
-            model, Cmd.none
-        else
-            let createAnswerData : Shared.Types.CreateAnswer =
-                { AuthorId = LocalStorage.Session.User.Id
-                  Content = model.Reply.Value }
-
-            if Validation.Show.verifyCreateAnswer createAnswerData then
-                { model with IsWaitingReply = true }, Cmd.ofPromise Rest.createAnswer (model.Question.Value.Id, createAnswerData) CreateAnswerSuccess NetworkError
-            else
-                model
-                |> Lens.set (Model.ReplyLens >-> StringField.ErrorLens) (Validation.Show.verifyAnswer model.Reply.Value), Cmd.none
-
-    | CreateAnswerSuccess result ->
-        match result.Code with
-        | Validation.QuestionNotFound ->
-            { model with IsWaitingReply = false }, Cmd.none
-
-        | Validation.Show.CreateAnswerSuccess ->
-            { model with IsWaitingReply = false
-                         Answers = model.Answers @ [ result.Data :?> Shared.Types.Answer ]
-                         Reply = StringField.Empty  }, Cmd.none
-
-        | code ->
-            Logger.errorFn "Unkown code: %O" code
-            { model with IsWaitingReply = false }, Cmd.none
--- elmish/src/Client/Question/Show/Types.fs
+++ elmish/src/Client/Question/Show/Types.fs
@@ -3,25 +3,10 @@ module Question.Show.Types
 open Okular.Lens
 
 type Model =
-    { Question : Shared.Types.Question option
-      Answers : Shared.Types.Answer list
-      Reply : StringField
-      IsWaitingReply : bool }
+    { Question : Shared.Types.Question option }
 
     static member Empty =
-        { Question =  None
-          Answers = []
-          Reply = StringField.Empty
-          IsWaitingReply = false }
-
-    static member ReplyLens =
-        { Get = fun (r : Model) -> r.Reply
-          Set = fun v (r : Model) -> { r with Reply = v } }
+        { Question =  None }
 
 type Msg =
-    | FetchDetail of int
-    | NetworkError of exn
-    | FetchDetailSuccess of Shared.Types.QuestionShow
-    | ChangeReply of string
-    | Submit
-    | CreateAnswerSuccess of Shared.Types.GenericJsonResponse
+    | None
