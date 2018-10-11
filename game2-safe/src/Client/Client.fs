e6193d2c11a1ae370263d7acf01c76df36ac7200
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-10-11 15:30:53 +0200

    dotnet new safe --js-deps npm --deploy azure --layout none

---
+++ game2-safe/src/Client/Client.fs
@@ -0,0 +1,105 @@
+module Client
+
+open Elmish
+open Elmish.React
+
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fable.PowerPack.Fetch
+
+open Thoth.Json
+
+open Shared
+
+
+
+// The model holds data that you want to keep track of while the application is running
+// in this case, we are keeping track of a counter
+// we mark it as optional, because initially it will not be available from the client
+// the initial value will be requested from server
+type Model = { Counter: Counter option }
+
+// The Msg type defines what events/actions can occur while the application is running
+// the state of the application changes *only* in reaction to these events
+type Msg =
+| Increment
+| Decrement
+| InitialCountLoaded of Result<Counter, exn>
+
+
+
+// defines the initial state and initial command (= side-effect) of the application
+let init () : Model * Cmd<Msg> =
+    let initialModel = { Counter = None }
+    let loadCountCmd =
+        Cmd.ofPromise
+            (fetchAs<Counter> "/api/init" Decode.int)
+            []
+            (Ok >> InitialCountLoaded)
+            (Error >> InitialCountLoaded)
+    initialModel, loadCountCmd
+
+// The update function computes the next state of the application based on the current state and the incoming events/messages
+// It can also run side-effects (encoded as commands) like calling the server via Http.
+// these commands in turn, can dispatch messages to which the update function will react.
+let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
+    match currentModel.Counter, msg with
+    | Some x, Increment ->
+        let nextModel = { currentModel with Counter = Some (x + 1) }
+        nextModel, Cmd.none
+    | Some x, Decrement ->
+        let nextModel = { currentModel with Counter = Some (x - 1) }
+        nextModel, Cmd.none
+    | _, InitialCountLoaded (Ok initialCount)->
+        let nextModel = { Counter = Some initialCount }
+        nextModel, Cmd.none
+
+    | _ -> currentModel, Cmd.none
+
+
+let safeComponents =
+    let components =
+        span [ ]
+           [
+             a [ Href "https://saturnframework.github.io" ] [ str "Saturn" ]
+             str ", "
+             a [ Href "http://fable.io" ] [ str "Fable" ]
+             str ", "
+             a [ Href "https://elmish.github.io/elmish/" ] [ str "Elmish" ]
+           ]
+
+    p [ ]
+        [ strong [] [ str "SAFE Template" ]
+          str " powered by: "
+          components ]
+
+let show = function
+| { Counter = Some x } -> string x
+| { Counter = None   } -> "Loading..."
+
+let view (model : Model) (dispatch : Msg -> unit) =
+    div []
+        [ h1 [] [ str "SAFE Template" ]
+          p  [] [ str "The initial counter is fetched from server" ]
+          p  [] [ str "Press buttons to manipulate counter:" ]
+          button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ]
+          div [] [ str (show model) ]
+          button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
+          safeComponents ]
+
+
+#if DEBUG
+open Elmish.Debug
+open Elmish.HMR
+#endif
+
+Program.mkProgram init update view
+#if DEBUG
+|> Program.withConsoleTrace
+|> Program.withHMR
+#endif
+|> Program.withReact "elmish-app"
+#if DEBUG
+|> Program.withDebugger
+#endif
+|> Program.run
---
+++ game2-safe/src/Server/Server.fs
@@ -0,0 +1,49 @@
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open FSharp.Control.Tasks.V2
+open Giraffe
+open Saturn
+open Shared
+
+open Giraffe.Serialization
+open Microsoft.WindowsAzure.Storage
+
+let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+let publicPath = tryGetEnv "public_path" |> Option.defaultValue "../Client/public" |> Path.GetFullPath
+let storageAccount = tryGetEnv "STORAGE_CONNECTIONSTRING" |> Option.defaultValue "UseDevelopmentStorage=true" |> CloudStorageAccount.Parse
+let port = 8085us
+
+let getInitCounter() : Task<Counter> = task { return 42 }
+
+let webApp = router {
+    get "/api/init" (fun next ctx ->
+        task {
+            let! counter = getInitCounter()
+            return! Successful.OK counter next ctx
+        })
+}
+
+let configureSerialization (services:IServiceCollection) =
+    let fableJsonSettings = Newtonsoft.Json.JsonSerializerSettings()
+    fableJsonSettings.Converters.Add(Fable.JsonConverter())
+    services.AddSingleton<IJsonSerializer>(NewtonsoftJsonSerializer fableJsonSettings)
+
+let configureAzure (services:IServiceCollection) =
+    tryGetEnv "APPINSIGHTS_INSTRUMENTATIONKEY"
+    |> Option.map services.AddApplicationInsightsTelemetry
+    |> Option.defaultValue services
+
+let app = application {
+    url ("http://0.0.0.0:" + port.ToString() + "/")
+    use_router webApp
+    memory_cache
+    use_static publicPath
+    service_config configureSerialization
+    service_config configureAzure
+    use_gzip
+}
+
+run app
